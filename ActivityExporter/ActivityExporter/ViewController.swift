09e35b370c5b0f10fb4eb300a7563740629404f2
Rémy LAVERGNE-PRUDENCE <lavergne.remy@gmail.com>
2019-08-18 13:14:14 +0200

    iOS project creation

---
+++ ActivityExporter/ActivityExporter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityExporter
+//
+//  Created by Rémy LAVERGNE-PRUDENCE on 18/08/2019.
+//  Copyright © 2019 Rémy LAVERGNE-PRUDENCE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
